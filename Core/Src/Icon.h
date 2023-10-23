89f5e27775df6793fc40c4ef30940ad55f6f5a6a
Polarist05 <88574175+Polarist05@users.noreply.github.com>
2023-10-23 08:18:35 +0700

    Initial commit

---
+++ Core/Src/Icon.h
@@ -0,0 +1,30 @@
+/*
+ * Icon.h
+ *
+ *  Created on: Oct 21, 2023
+ *      Author: vorapolpromwan
+ */
+#pragma once
+
+#define BURST_AREA_ICON 0xF800
+#include "main.h"
+
+#ifndef SRC_ICON_H_
+#define SRC_ICON_H_
+extern const uint8_t playerImage1[512];
+extern const uint8_t playerImage2[512];
+extern const uint8_t playerImage3[512];
+extern const uint8_t playerImage4[512];
+extern const uint8_t playerImage1Bomb[512];
+extern const uint8_t playerImage2Bomb[512];
+extern const uint8_t playerImage3Bomb[512];
+extern const uint8_t playerImage4Bomb[512];
+extern const uint8_t grassImage[512];
+extern const uint8_t bombImage[512];
+extern const uint8_t bootImage[512];
+extern const uint8_t heartImage[512];
+extern const uint8_t potionImage[512];
+extern const uint8_t addBombImage[512];
+extern const uint8_t wallImage1[512];
+extern const uint8_t wallImage2[512];
+#endif /* SRC_ICON_H_ */
